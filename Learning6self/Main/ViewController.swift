d31998ad3f00ee3242f7a9a3a648206ad1c62ba9
Константин Кифа <pandavandama@MacBook-Air-Konstantin.local>
2022-05-16 22:43:49 +0300

    init

---
+++ Learning6self/Main/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Learning6self
+//
+//  Created by Константин Кифа on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func button1Touch(_ sender: UIButton) {
+        print("Testim!")
+    }
+    
+}
+
